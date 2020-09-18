a89d11903854e3004edf46525db24e8e45942151
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2020-09-18 19:15:57 +0800

    Update parking_Broken.py

--- parking/parking_Broken.py
+++ parking/parking_Broken.py
@@ -4,8 +4,7 @@
 
 import sys
 
-rates = [0]
-rates.extend(list(map(int,sys.stdin.readline().split())))
+rates = list(map(int,sys.stdin.readline().split()))
 trucks = [list(map(int,sys.stdin.readline().split())) for _ in range(3)]
 cost = 0
 parked_trucks = 0
@@ -17,7 +16,7 @@ for t in range(1,100):
   #if truck leaves
   if t == trucks[0][1] or t == trucks[1][1] or t == trucks[2][1]:
     parked_trucks -= 1
-  cost += rates[parked_trucks]*parked_trucks
+  cost += rates[parked_trucks-1]*parked_trucks
   # print(parked_trucks,rates[parked_trucks],rates[parked_trucks]*parked_trucks,cost)
 
 print(cost)
